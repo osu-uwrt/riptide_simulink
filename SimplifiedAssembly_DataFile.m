c6cd4cdb2f8227154b35c0b90072b0ab457a4ca6
spyalexs <60529991+spyalexs@users.noreply.github.com>
2022-09-13 21:41:46 -0400

    Add files via upload

---
+++ SimplifiedAssembly_DataFile.m
@@ -0,0 +1,188 @@
+% Simscape(TM) Multibody(TM) version: 7.5
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(18).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(18).angle = 0.0;
+smiData.RigidTransform(18).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(18).ID = "";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [7.0143518290201383 16.226306167718469 -38.041366299243158];  % in
+smiData.RigidTransform(1).angle = 3.1415926535897927;  % rad
+smiData.RigidTransform(1).axis = [-0.9240194808251887 -8.2635116372346067e-17 0.3823453923555884];
+smiData.RigidTransform(1).ID = "B[TempestFullAssembly-1:-:T200-THRUSTER-R1-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [-0.8749999999996585 1.9291338582677149 2.0432371408562258];  % in
+smiData.RigidTransform(2).angle = 1.5707963267948981;  % rad
+smiData.RigidTransform(2).axis = [1 -2.3551386880256609e-16 -3.9252311467094348e-16];
+smiData.RigidTransform(2).ID = "F[TempestFullAssembly-1:-:T200-THRUSTER-R1-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [30.472711478266763 16.226306167718459 -24.604780175336003];  % in
+smiData.RigidTransform(3).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(3).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(3).ID = "B[TempestFullAssembly-1:-:T200-THRUSTER-R1-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [10.863961030678936 1.9291338582677184 5.2821981715351694];  % in
+smiData.RigidTransform(4).angle = 1.7180331428203144;  % rad
+smiData.RigidTransform(4).axis = [0.86262802976710573 -0.35768203914938279 0.35768203914938279];
+smiData.RigidTransform(4).ID = "F[TempestFullAssembly-1:-:T200-THRUSTER-R1-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [13.898821733164301 16.226306167718459 -24.616907761841368];  % in
+smiData.RigidTransform(5).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(5).axis = [-0.57735026918962562 -0.57735026918962562 -0.57735026918962595];
+smiData.RigidTransform(5).ID = "B[TempestFullAssembly-1:-:T200-THRUSTER-R1-3]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [10.863961030678905 1.9291338582677224 5.2821981715352102];  % in
+smiData.RigidTransform(6).angle = 2.5930677166556015;  % rad
+smiData.RigidTransform(6).axis = [0.28135253610852956 0.67854283226090384 -0.67854283226090417];
+smiData.RigidTransform(6).ID = "F[TempestFullAssembly-1:-:T200-THRUSTER-R1-3]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [13.898821733164301 16.226306167718459 -24.616907761841368];  % in
+smiData.RigidTransform(7).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(7).axis = [-0.57735026918962562 -0.57735026918962562 -0.57735026918962595];
+smiData.RigidTransform(7).ID = "B[TempestFullAssembly-1:-:T200-THRUSTER-R1-4]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(8).translation = [-10.863961030678924 1.9291338582677255 5.2821981715351729];  % in
+smiData.RigidTransform(8).angle = 2.5940608158810847;  % rad
+smiData.RigidTransform(8).axis = [0.28081675480862245 -0.67865379620936106 0.67865379620936117];
+smiData.RigidTransform(8).ID = "F[TempestFullAssembly-1:-:T200-THRUSTER-R1-4]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(9).translation = [37.376820115875518 20.726306167718462 -38.019149217657102];  % in
+smiData.RigidTransform(9).angle = 2.5930677166556046;  % rad
+smiData.RigidTransform(9).axis = [0.67854283226090428 0.67854283226090428 0.28135253610852806];
+smiData.RigidTransform(9).ID = "B[TempestFullAssembly-1:-:T200-THRUSTER-R1-5]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(10).translation = [0.87499999999999623 1.9291338582677195 -2.4567628591437707];  % in
+smiData.RigidTransform(10).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(10).axis = [0.57735026918962562 0.57735026918962551 -0.57735026918962618];
+smiData.RigidTransform(10).ID = "F[TempestFullAssembly-1:-:T200-THRUSTER-R1-5]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(11).translation = [8.2312441638465792 16.226306167718455 -9.9641967162147136];  % in
+smiData.RigidTransform(11).angle = 1.7180331428203164;  % rad
+smiData.RigidTransform(11).axis = [-0.35768203914938435 -0.35768203914938468 0.86262802976710429];
+smiData.RigidTransform(11).ID = "B[TempestFullAssembly-1:-:T200-THRUSTER-R1-6]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(12).translation = [-0.87500000000005151 1.9291338582677349 2.0432371408562138];  % in
+smiData.RigidTransform(12).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(12).axis = [0.57735026918962584 0.57735026918962562 -0.57735026918962584];
+smiData.RigidTransform(12).ID = "F[TempestFullAssembly-1:-:T200-THRUSTER-R1-6]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(13).translation = [36.11883937910541 20.726306167718452 -9.943790569643145];  % in
+smiData.RigidTransform(13).angle = 0.78466643508699663;  % rad
+smiData.RigidTransform(13).axis = [5.316205763301278e-16 1 2.1997661527625576e-16];
+smiData.RigidTransform(13).ID = "B[TempestFullAssembly-1:-:T200-THRUSTER-R1-7]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(14).translation = [0.87499999999987388 1.9291338582677033 -2.4567628591437738];  % in
+smiData.RigidTransform(14).angle = 3.1415926535897927;  % rad
+smiData.RigidTransform(14).axis = [-2.7755575615628914e-17 -0.70710678118654735 0.70710678118654779];
+smiData.RigidTransform(14).ID = "F[TempestFullAssembly-1:-:T200-THRUSTER-R1-7]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(15).translation = [30.472711478266763 16.226306167718459 -24.604780175336003];  % in
+smiData.RigidTransform(15).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(15).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(15).ID = "B[TempestFullAssembly-1:-:T200-THRUSTER-R1-8]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(16).translation = [-10.489945282647414 1.929133858267718 5.6562139195667172];  % in
+smiData.RigidTransform(16).angle = 1.7175100935868697;  % rad
+smiData.RigidTransform(16).axis = [0.86308426487943013 0.35713131458717928 -0.35713131458717928];
+smiData.RigidTransform(16).ID = "F[TempestFullAssembly-1:-:T200-THRUSTER-R1-8]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(17).translation = [-38.255108672846021 7.4429148097891389 60.088278726033991];  % in
+smiData.RigidTransform(17).angle = 0;  % rad
+smiData.RigidTransform(17).axis = [0 0 0];
+smiData.RigidTransform(17).ID = "RootGround[TempestFullAssembly-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(18).translation = [-38.255108672846021 28.669220977507585 60.088278726033998];  % in
+smiData.RigidTransform(18).angle = 1.5707963267948961;  % rad
+smiData.RigidTransform(18).axis = [-1 0 0];
+smiData.RigidTransform(18).ID = "SixDofRigidTransform[Orgin^SimplifiedAssembly-1]";
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(3).mass = 0.0;
+smiData.Solid(3).CoM = [0.0 0.0 0.0];
+smiData.Solid(3).MoI = [0.0 0.0 0.0];
+smiData.Solid(3).PoI = [0.0 0.0 0.0];
+smiData.Solid(3).color = [0.0 0.0 0.0];
+smiData.Solid(3).opacity = 0.0;
+smiData.Solid(3).ID = "";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 0.35180870288029081;  % kg
+smiData.Solid(1).CoM = [-0.0013498775752404913 0.025530974617350745 0.29345364512485378];  % in
+smiData.Solid(1).MoI = [0.46121519676781619 0.44828277391374055 0.51063039728245951];  % kg*in^2
+smiData.Solid(1).PoI = [0.001924878403160672 -0.00066580021326222844 0.00019354737585307866];  % kg*in^2
+smiData.Solid(1).color = [0.48588235294117643 0.068627450980392149 0.068627450980392149];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = "T200-THRUSTER-R1*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(2).mass = 20.056425426330314;  % kg
+smiData.Solid(2).CoM = [22.211871297527978 16.204758007906705 -23.668289997745802];  % in
+smiData.Solid(2).MoI = [728.09345439261642 1576.5476058673255 1466.7037182476972];  % kg*in^2
+smiData.Solid(2).PoI = [-16.02690525327969 -0.52846564514092487 -0.89184711517874304];  % kg*in^2
+smiData.Solid(2).color = [0.75294117647058822 0.75294117647058822 0.75294117647058822];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = "TempestFullAssembly*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(3).mass = 1.6409721710573928e-06;  % kg
+smiData.Solid(3).CoM = [36.14028904758441 39.257491220962685 0];  % in
+smiData.Solid(3).MoI = [5.9827110403136396e-10 5.9827110403136396e-10 5.1280380345548114e-10];  % kg*in^2
+smiData.Solid(3).PoI = [0 0 0];  % kg*in^2
+smiData.Solid(3).color = [0.69803921568627447 0.69803921568627447 0.69803921568627447];
+smiData.Solid(3).opacity = 1;
+smiData.Solid(3).ID = "Orgin^SimplifiedAssembly*:*Default";
+
