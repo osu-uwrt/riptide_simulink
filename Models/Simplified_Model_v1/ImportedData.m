6d7c41a08e387b28e83204ab5e69517a92d3f795
spyalexs <spyalexs@gmail.com>
2022-09-18 15:10:27 -0400

    Fixed Robot Intial Cofiguration

--- Models/Simplified_Model_v1/ImportedData.m
+++
@@ -1,8 +0,0 @@
-%Data to be configured
-%This file should be run before starting the model
-
-%The robot's orgin is on the back left wing!!!!!
-%Using let hand frame X+ left, Y+ forward, Z+ down
-
-%Robot Center of Bouancy-------------------------
-importedData.robot.cob = [-0.41001, 0.3811, .10810]; %m
